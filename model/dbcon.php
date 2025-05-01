9d29c206447e96eb989b95ddee1f3da62a336d63
Amr Khaled <redwolf4325@gmail.com>
2025-05-02 01:16:08 +0300

    Just confgured somethings to work with me, and those changed caused by my prettier and for my php extension to figure out what where

--- model/dbcon.php
+++ model/dbcon.php
@@ -1,9 +1,9 @@
 <?php
 require_once("./config.php");
-$cn= mysqli_connect(DB_HOST_NAME,DB_USER_NAME,DB_USER_PASSWORD,DB_NAME);
-if(!$cn){
-  die("connection faild: ".mysqli_connect_error());
-  mysqli_close($cn);
+/**@var mysqli */
+$cn = mysqli_connect(DB_HOST_NAME, DB_USER_NAME, DB_USER_PASSWORD, DB_NAME);
+if (!$cn) {
+  die("connection failed: " . mysqli_connect_error());
+  # mysqli_close($cn);
 }
 
-?>
