212485b84712bd0b4e3ef38bf8deb8f2f715b258
Amr Khaled <redwolf4325@gmail.com>
2025-05-04 01:30:58 +0300

    Tried but Tired

--- index.php
+++ index.php
@@ -3,9 +3,15 @@
 require_once __DIR__ . "/vendor/autoload.php";
 require_once __DIR__ . "/config.php";
 
-use Model\Admin;
 
 var_dump($_SERVER["REQUEST_URI"]);
 var_dump($_SERVER["REQUEST_METHOD"]);
-$_COOKIE["NAME"] = "ss";
-var_dump($_COOKIE);
+
+$uri = $_SERVER["REQUEST_URI"];
+$uri = explode(".", $uri);
+$dir = scandir(__DIR__ . "/pages/");
+var_dump($dir);
+function view(string $uri)
+{
+  dir(__DIR__ . "/pages");
+}
