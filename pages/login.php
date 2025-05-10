eef74285f482ace6b5f557be1b54f54be49227e3
hh11909 <hamo1190927@gmail.com>
2025-05-10 04:24:30 +0300

    modifying in pages/login.php and adding handle-login.js

--- pages/login.php
+++ pages/login.php
@@ -1,99 +1,38 @@
+<?php
+use controller\Student;
+session_start();
+require_once(__DIR__."/../controller/Student.php");
+if(isset($_SESSION["user"])){
+  /**@var Student $user */
+  $user=unserialize($_SESSION["user"]);
+  $role=$user->role;
+  if($role=="student"){
+    header("Location:index.php");
+  }
+  elseif($role=="admin"){
+    header("Location:index.php");
+  }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>BATU Library</title>
   <link rel="stylesheet" href="css/bootstrap.min.css">
   <link rel="stylesheet" href="css/all.min.css">
-
   <link rel="stylesheet" href="css/style.css">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link
     href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Quicksand:wght@300..700&display=swap"
     rel="stylesheet">
-
-
 </head>
 
 <body class="register-body">
   <!-- navigation bar start -->
-  <nav class="navbar navbar-expand-lg navbar-dark custom-navbar fixed-top p-1">
-    <div class="container">
-      <!-- logo -->
-      <a class="navbar-brand fs-4 " href="index.php"><img src="images/logo.png" alt="Logo" width="48" height="48"
-          class="me-2 p-1 logo">
-        <span class="logo-title">
-          BATU Library
-        </span></a>
-      <!-- toggle button -->
-      <button class="navbar-toggler shadow-none" type="button" data-bs-toggle="offcanvas"
-        data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <!-- sidebar -->
-      <div class="sidebar offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar"
-        aria-labelledby="offcanvasNavbarLabel">
-        <!-- sidebar header -->
-        <div class="offcanvas-header text-white border-bottom">
-          <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Discover</h5>
-          <button type="button" class="btn-close btn-close-white shadow-none" data-bs-dismiss="offcanvas"
-            aria-label="Close"></button>
-        </div>
-        <!-- sidebar body -->
-        <div class="offcanvas-body d-flex flex-column flex-lg-row p-lg-0 p-4">
-          <ul class="navbar-nav justify-content-lg-end align-items-center fs-6 flex-grow-1 pe-3">
-            <li class="nav-item d-flex align-items-center d-block d-lg-none mb-3">
-              <a href="profile.php"><img src="wishlist-images/profile.png" alt="User" class="rounded-circle ms-3"
-                  width="40" height="40"></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link mx-2" aria-current="page" href="index.php">Home</a>
-            </li>
-            <li class="nav-item mx-2">
-              <a class="nav-link" href="about.php">About</a>
-            </li>
-            <li class="nav-item mx-2">
-              <a class="nav-link" href="contact.php">Contact</a><!--to do-->
-            </li>
-            <li class="nav-item dropdown mx-2">
-              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                aria-expanded="false">
-                Services
-              </a>
-              <ul class="dropdown-menu mt-3">
-                <!-- COMMENT: I think the hover text color has low contrast  -->
-                <li><a class="dropdown-item" href="Explore.php">Explore</a></li>
-
-                <li>
-                  <a class="dropdown-item" href="Events.php">Events</a><!--to do-->
-                </li>
-                <li>
-                  <a class="dropdown-item" href="wishlist.php">Wishlist</a>
-                </li>
-                <li>
-                  <a class="dropdown-item" href="borrowed.php">Borrowed</a>
-                </li>
-              </ul>
-            </li>
-          </ul>
-          <!-- login/signup -->
-          <div class="d-flex justify-content-center align-items-center ">
-            <a href="login.php" id="login" class="text-white fw-semibold text-decoration-none px-3 py-1 rounded-4">Log
-              In</a>
-              <!-- removed the register  //omar -->
-          </div>
-          <!-- profile -->
-          <div class="d-flex align-items-center mt-1 d-none d-lg-block">
-            <a href="profile.php"><img src="wishlist-images/profile.png" alt="User" class="rounded-circle ms-3"
-                width="40" height="40"></a><!--to do-->
-          </div>
-        </div>
-      </div>
-    </div>
-  </nav>
+  <?php require_once('nav.php')?>
   <div style="height: 66px;"></div>
   <!-- navigation bar end -->
 
@@ -119,19 +58,21 @@
           <p>
 
             <label class="field-label " for="email">Email</label>
-            <input type="email" id="email" name="email" class="form-control rounded-pill py-2" required />
+            <input type="email" id="email" name="email" class="form-control rounded-pill py-2"  />
           </p>
+          <p id="email-error" style="color:red;"></p>
           <div>
             <label class="field-label" for="password">Password</label>
             <div class="input-container">
-              <input type="password" id="password" name="password" class="form-control rounded-pill py-2 " required />
+              <input type="password" id="password" name="password" class="form-control rounded-pill py-2 "  />
               <span class="togglePassword" id="togglePassword"
-                onclick="toggleIcon(this.previousElementSibling,this.children[0],this.children[1])">
-                <i class="fa-solid fa-eye-slash" id="icon1" style="cursor: pointer; display: inline-block"></i>
-                <i class="fa-solid fa-eye" id="icon2" style="cursor: pointer;display: none;"></i>
-              </span>
-            </div>
+              onclick="toggleIcon(this.previousElementSibling,this.children[0],this.children[1])">
+              <i class="fa-solid fa-eye-slash" id="icon1" style="cursor: pointer; display: inline-block"></i>
+              <i class="fa-solid fa-eye" id="icon2" style="cursor: pointer;display: none;"></i>
+            </span>
+            <p id="pass-error" style="color:red;"></p>
           </div>
+        </div>
           <a class="forgot-password" href="#">Forgot Password?</a>
           <div class="text-center">
 
@@ -212,6 +153,7 @@
   <!-- js -->
   <script src="js/index.js"></script>
   <script src="assets/js/main.js"></script>
+  <script src="js/handle-login.js"></script>
 </body>
 
 </html>
