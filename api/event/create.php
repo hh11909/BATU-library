8a1ea2fb4e6c7876320b3d5306335cab4238e01c
Amr Khaled <redwolf4325@gmail.com>
2025-05-12 17:52:12 +0300

    update

--- api/event/create.php
+++ api/event/create.php
@@ -15,10 +15,11 @@ header("Access-Control-Allow-Origin:*");
 header("Access-Control-Allow-Methods:POST");
 header("Access-Control-Allow-Headers:Content-Type,Authorization,X-Request-With");
 $requestMethod = $_SERVER["REQUEST_METHOD"];
-$inputData = json_decode(file_get_contents("php://input"), true);
+
 if ($requestMethod == "POST") {
   if (!isset($_SESSION['user'])) {
-    echo "login";
+    header('Location: /pages/login.php');
+    error422('Log in Please', 403);
     die();
   }
   /*@var User $user*/
@@ -32,38 +33,10 @@ if ($requestMethod == "POST") {
   );
   if ($user instanceof Admin) {
     $evn->setAdmin_ID($user->id);
-    $user->createEvent($evn);
-    echo "ADMIN";
+    echo $user->createEvent($evn);
   }
   if ($user instanceof Friend) {
     $evn->student_ID = $user->id;
-    $user->createEvent($evn);
-    echo "FRIEND";
-  }
-  die();
-  // $new_event=new Event();
-  // Admin :: createEvent( Event $new_event,$role,$admin_ID)
-  // {
-  // $role= "admin";
-  // $new_event->create($role)
-  // }
-  var_dump($_POST);
-  die();
-  $data = json_decode(file_get_contents("php://input"), true);
-  $title = mysqli_real_escape_string($conn, $data['title']);
-  $start_date = mysqli_real_escape_string($conn, $data['start_date']);
-  $end_date = mysqli_real_escape_string($conn, $data['end_date']);
-  $content = mysqli_real_escape_string($conn, $data['content']);
-  if ($role == 'admin' || $role == 'friend') {
-    $state = 'available';
-  } else {
-    $state = 'requested';
-    $query = "INSERT INTO events (title, start_date, end_date, content, state)
-          VALUES ('$title', '$start_date', '$end_date', '$content', $state)";
-    if (mysqli_query($conn, $query)) {
-      echo json_encode(["message" => "Event created successfully"]);
-    } else {
-      echo json_encode(["error" => "Error: " . mysqli_error($conn)]);
-    }
+    echo $user->createEvent($evn);
   }
 }
