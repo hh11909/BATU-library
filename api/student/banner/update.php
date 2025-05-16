8c8081857d7ae16f31da0d208c26a70589f7670b
hh11909 <hamo1190927@gmail.com>
2025-05-16 03:23:26 +0300

    friend can now update the profile and banner images

--- api/student/banner/update.php
+++ api/student/banner/update.php
@@ -1,8 +1,10 @@
 <?php
 require_once("../../../controller/Student.php");
+require_once("../../../controller/Friend.php");
 require_once("../../../controller/Admin.php");
 
 use controller\Student;
+use controller\Friend;
 session_start();
 header("Content-Type:application/json");
 header("Access-Control-Allow-Origin:*");
@@ -11,8 +13,14 @@ header("Access-Control-Allow-Headers:Content-Type,Authorization,X-Request-With")
 
 if ($_SERVER["REQUEST_METHOD"]=="POST") {
   if(isset($_SESSION["user"])){
+    if($_SESSION["role"]=="student"){
     /**@var Student $user*/
     $user=unserialize($_SESSION["user"]);
+    }
+    elseif($_SESSION["role"]=="friend"){
+    /**@var Friend $user*/
+    $user=unserialize($_SESSION["user"]);
+    }
     if($user->role!="student"){
       echo error422("you are not authorized to be here!");
       header("Location:../../pages/login.php");
