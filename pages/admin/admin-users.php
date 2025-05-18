c76baf239a6e966dd6d7388b8332e16434ac250e
Amr Khaled <redwolf4325@gmail.com>
2025-05-18 11:35:06 +0300

    Fixed

--- pages/admin/admin-users.php
+++ pages/admin/admin-users.php
@@ -11,6 +11,7 @@
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="../css/admin-style.css" />
+  <link rel="stylesheet" href="../css/form.css" />
 </head>
 <?php require_once("admin-header.php"); ?>
 <!-- Admin Sidebar -->
@@ -57,111 +58,114 @@
           </select>
         </div>
       </div>
+    </div>
 
-      <!-- Stats Cards -->
-      <div class="row">
-        <div class="col-md-4 col-sm-6 col-6">
-          <div class="stat-card">
-            <i class="fa-solid fa-user-tie"></i>
-            <h3>5</h3>
-            <p>Total admins</p>
-          </div>
-        </div>
-        <div class="col-md-4 col-sm-6 col-6">
-          <div class="stat-card">
-            <i class="fa-solid fa-user"></i>
-            <h3 id="total-students"></h3>
-            <p>Total students</p>
-          </div>
-        </div>
-        <div class="col-md-4 col-sm-6 col-6">
-          <div class="stat-card">
-            <i class="fa-solid fa-user-group"></i>
-            <h3 id="total-friends"></h3>
-            <p>Total Freinds</p>
-          </div>
+    <!-- Stats Cards -->
+    <div class="row">
+      <div class="col-md-4 col-sm-6 col-6">
+        <div class="stat-card">
+          <i class="fa-solid fa-user-tie"></i>
+          <h3>5</h3>
+          <p>Total admins</p>
         </div>
       </div>
-
-      <!-- users Table -->
-      <div class="card mt-4">
-        <div class="card-header bg-primary-color text-white d-flex justify-content-between align-items-center">
-          <h5 class="mb-0"><i class="fas fa-list me-2"></i> Students table </h5>
-          <div class="dropdown" id="borrowed-dropdown">
-            <button class="btn btn-sm btn-light dropdown-toggle  primary-color" type="button" id="filterDropdown" data-bs-toggle="dropdown" aria-expanded="false">
-              Department
-            </button>
-            <ul class="dropdown-menu dropdown-menu-end" id="borrowed-dropdown-menu" aria-labelledby="filterDropdown">
-              <li><a class="dropdown-item" href="#">ID</a></li>
-              <li><a class="dropdown-item" href="#">Name</a></li>
-              <li><a class="dropdown-item" href="#">Phone</a></li>
-            </ul>
-          </div>
-          <div class="dropdown" id="borrowed-dropdown">
-            <button class="btn btn-sm btn-light dropdown-toggle  primary-color" type="button" id="filterDropdown" data-bs-toggle="dropdown" aria-expanded="false">
-              College
-            </button>
-            <ul class="dropdown-menu dropdown-menu-end" id="borrowed-dropdown-menu" aria-labelledby="filterDropdown">
-              <li><a class="dropdown-item" href="#">ID</a></li>
-              <li><a class="dropdown-item" href="#">Name</a></li>
-              <li><a class="dropdown-item" href="#">Phone</a></li>
-            </ul>
-          </div>
-          <div class="dropdown" id="borrowed-dropdown">
-            <button class="btn btn-sm btn-light dropdown-toggle  primary-color" type="button" id="filterDropdown" data-bs-toggle="dropdown" aria-expanded="false">
-              Academic year
-            </button>
-            <ul class="dropdown-menu dropdown-menu-end" id="borrowed-dropdown-menu" aria-labelledby="filterDropdown">
-              <li><a class="dropdown-item" href="#">all</a></li>
-              <li><a class="dropdown-item" href="#">1</a></li>
-              <li><a class="dropdown-item" href="#">2</a></li>
-              <li><a class="dropdown-item" href="#">3</a></li>
-              <li><a class="dropdown-item" href="#">4</a></li>
-            </ul>
-          </div>
+      <div class="col-md-4 col-sm-6 col-6">
+        <div class="stat-card">
+          <i class="fa-solid fa-user"></i>
+          <h3 id="total-students"></h3>
+          <p>Total students</p>
         </div>
-        <div class="card-body px-md-1">
-          <div class="table-responsive">
-            <table class="table table-hover">
-              <thead>
-                <tr>
-                  <th>User ID</th>
-                  <th>Name</th>
-                  <th>E-mail</th>
-                  <th>Phone</th>
-                  <th>Friend</th>
-                  <th class="text-center">Department</th>
-                  <th class="text-center">Academec year</th>
-                  <th>Delet user</th>
-                </tr>
-              </thead>
-              <tbody id="stu-table">
-                <tr>
-                  <td>2320100</td>
-                  <td>Omar Mostafa</td>
-                  <td>2320100@gmail.com</td>
-                  <td>0123456789</td>
-                  <td>Yes</td>
-                  <td class="text-center">IT</td>
-                  <td class="text-center">2nd</td>
-                  <td>
-                    <button class="btn btn-sm btn-danger"><i class="fa-solid fa-trash"></i></i></button>
-                  </td>
-                </tr>
-
-
-              </tbody>
-            </table>
-          </div>
+      </div>
+      <div class="col-md-4 col-sm-6 col-6">
+        <div class="stat-card">
+          <i class="fa-solid fa-user-group"></i>
+          <h3 id="total-friends"></h3>
+          <p>Total Freinds</p>
         </div>
       </div>
     </div>
 
-    <?php require_once("admin-footer.php") ?>
+    <!-- users Table -->
+    <div class="card mt-4">
+      <div class="card-header bg-primary-color text-white d-flex justify-content-between align-items-center">
+        <h5 class="mb-0"><i class="fas fa-list me-2"></i> Students table </h5>
+        <div class="dropdown" id="borrowed-dropdown">
+          <button class="btn btn-sm btn-light dropdown-toggle  primary-color" type="button" id="filterDropdown" data-bs-toggle="dropdown" aria-expanded="false">
+            Department
+          </button>
+          <ul class="dropdown-menu dropdown-menu-end" id="borrowed-dropdown-menu" aria-labelledby="filterDropdown">
+            <li><a class="dropdown-item" href="#">ID</a></li>
+            <li><a class="dropdown-item" href="#">Name</a></li>
+            <li><a class="dropdown-item" href="#">Phone</a></li>
+          </ul>
+        </div>
+        <div class="dropdown" id="borrowed-dropdown">
+          <button class="btn btn-sm btn-light dropdown-toggle  primary-color" type="button" id="filterDropdown" data-bs-toggle="dropdown" aria-expanded="false">
+            College
+          </button>
+          <ul class="dropdown-menu dropdown-menu-end" id="borrowed-dropdown-menu" aria-labelledby="filterDropdown">
+            <li><a class="dropdown-item" href="#">ID</a></li>
+            <li><a class="dropdown-item" href="#">Name</a></li>
+            <li><a class="dropdown-item" href="#">Phone</a></li>
+          </ul>
+        </div>
+        <div class="dropdown" id="borrowed-dropdown">
+          <button class="btn btn-sm btn-light dropdown-toggle  primary-color" type="button" id="filterDropdown" data-bs-toggle="dropdown" aria-expanded="false">
+            Academic year
+          </button>
+          <ul class="dropdown-menu dropdown-menu-end" id="borrowed-dropdown-menu" aria-labelledby="filterDropdown">
+            <li><a class="dropdown-item" href="#">all</a></li>
+            <li><a class="dropdown-item" href="#">1</a></li>
+            <li><a class="dropdown-item" href="#">2</a></li>
+            <li><a class="dropdown-item" href="#">3</a></li>
+            <li><a class="dropdown-item" href="#">4</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="card-body px-md-1">
+        <div class="table-responsive">
+          <table class="table table-hover">
+            <thead>
+              <tr>
+                <th>User ID</th>
+                <th>Name</th>
+                <th>E-mail</th>
+                <th>Phone</th>
+                <th>Friend</th>
+                <th class="text-center">Department</th>
+                <th class="text-center">Academec year</th>
+                <th>Delet user</th>
+              </tr>
+            </thead>
+            <tbody id="stu-table">
+              <tr>
+                <td>2320100</td>
+                <td>Omar Mostafa</td>
+                <td>2320100@gmail.com</td>
+                <td>0123456789</td>
+                <td>Yes</td>
+                <td class="text-center">IT</td>
+                <td class="text-center">2nd</td>
+                <td>
+                  <button class="btn btn-sm btn-danger"><i class="fa-solid fa-trash"></i></i></button>
+                </td>
+              </tr>
 
-    <!-- Bootstrap JS -->
-    <script src="../js/handle-user.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    </body>
+
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+<?php require_once("admin-footer.php") ?>
+
+<!-- Bootstrap JS -->
+<script src="../js/handle-user.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
 
 </html>
